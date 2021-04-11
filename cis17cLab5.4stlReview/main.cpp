237384e25b1a6145ec2d8ba6883e0e8f6fef43d6
Jason Jones <jasojone@gmail.com>
2021-04-11 16:48:13 -0700

    lab 5 stl work

--- cis17cLab5.4stlReview/main.cpp
+++ cis17cLab5.4stlReview/main.cpp
@@ -6,15 +6,43 @@
 
 #include <queue>
 #include <iostream>
-
 using namespace std;
-
-/*
- * 
- */
-int main(int argc, char** argv)
+// printQue will print the queue 
+// while queue is not empty loop
+void printQue(queue<int> queCopy)
 {
-
+    queue<int> que = queCopy;
+    
+    while (!que.empty()) {
+        cout << " " << que.front() << ",";
+        que.pop();
+    }
+    cout << '\n';
+}
+  
+int main()
+{
+    queue<int> qRev;
+    // use push to fill the queue
+    qRev.push(10);
+    qRev.push(20);
+    qRev.push(30);
+  
+    cout << "The queue qRevs contents are: ";
+    printQue(qRev);
+  
+    
+    cout << "\nqRev.size() : " << qRev.size();
+    
+    cout << "\nqRev.front() : " << qRev.front();
+    
+    cout << "\nqRev.back() : " << qRev.back();
+  
+    cout << "\nqRev.pop() : ";
+    
+    qRev.pop();
+    
+    printQue(qRev);
+  
     return 0;
 }
-
