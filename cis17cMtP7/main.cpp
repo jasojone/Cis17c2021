b08f9c6c64349ea2a783660afa72476ecd80a592
Jason Jones <jasojone@gmail.com>
2021-05-17 16:19:02 -0700

    commit

--- cis17cMtP7/main.cpp
+++ cis17cMtP7/main.cpp
@@ -11,15 +11,14 @@ int main ()
     srand(static_cast<unsigned int>(time(0)));
    
     int faces = 13,
-        loop = 600000, 
-        count = 0;
+        loopCnt = 600000; 
     
     float three = 0, 
           four = 0, 
           twoPair = 0, 
           onePair = 0;
 
-    for(int i = 0; i < loop; ++i)
+    for(int i = 0; i < loopCnt; ++i)
     {
         vector<int> deck(13, 0);
         int pairs = 0;
@@ -29,9 +28,10 @@ int main ()
             deck[rand()%13]++;
         }
 
-        for(int j = 0; j < deck.size(); ++j)
+        for(int i = 0; i < deck.size(); ++i)
         {
-            switch(deck[j]) 
+         
+            switch(deck[i]) 
             {
                 case 2: 
                     ++pairs;
@@ -56,10 +56,10 @@ int main ()
     }
     
     cout << "Simulated Probability of Card Combinations: " << endl;
-    cout << "One Pair:       " << onePair/loop * 100 << "%" << endl;
-    cout << "Two Pair:       " << twoPair/loop * 100 << "%" << endl;
-    cout << "Three Pair:     " << three/loop * 100 << "%" << endl;
-    cout << "Four of a kind: " << four/loop * 100 << "%" << endl;
+    cout << "One Pair:       " << onePair/loopCnt * 100 << "%" << endl;
+    cout << "Two Pair:       " << twoPair/loopCnt * 100 << "%" << endl;
+    cout << "Three Pair:     " << three/loopCnt * 100 << "%" << endl;
+    cout << "Four of a kind: " << four/loopCnt * 100 << "%" << endl;
     
     return 0;
 }
