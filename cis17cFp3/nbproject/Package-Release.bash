8a48ffa0089cb0edb10d02fcde7dc6a13abf1bee
Jason Jones <jasojone@gmail.com>
2021-06-07 11:19:28 -0700

    commit

--- cis17cFp3/nbproject/Package-Release.bash
+++ cis17cFp3/nbproject/Package-Release.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cis17cfp1
-OUTPUT_BASENAME=cis17cfp1
-PACKAGE_TOP_DIR=cis17cfp1/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cis17cfp3
+OUTPUT_BASENAME=cis17cfp3
+PACKAGE_TOP_DIR=cis17cfp3/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/cis17cfp1/bin"
+makeDirectory "${NBTMPDIR}/cis17cfp3/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/cis17cfp1.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/cis17cfp3.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/cis17cfp1.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/cis17cfp3.tar *
 checkReturnCode
 
 # Cleanup
